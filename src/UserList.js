015aa3c5ebf2b6f0028e56216feeeef9214fcb42
kjh6511 <jinhee6511@naver.com>
2022-03-17 15:02:33 +0900

    react todoList study

---
+++ src/UserList.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function User({user,onRemove}){
+    const {username,email,id} = user;
+    return(
+    <div>
+        <b>{username}</b> <span>({email})</span>
+        <button onClick={() => onRemove(id)}>삭제</button>
+    </div>
+    );
+}
+
+
+
+function UserList({users, onRemove }){
+
+    return(
+        <div>
+            {
+                users.map(
+                    (user, index) =>(
+                        <User 
+                            user={user} 
+                            key={user.id} 
+                            onRemove={onRemove}
+                        />
+                    )
+                )
+            }
+        </div>
+    );
+}
+
+export default UserList;
