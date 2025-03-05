46694f959b6b2e010ddb68d80f7bb1610f543bc0
kjh6511 <jinhee6511@naver.com>
2025-03-05 17:51:43 +0900

    mealgo start

---
+++ src/main/java/com/mealgo/MealgoApplication.java
@@ -0,0 +1,13 @@
+package com.mealgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mealgo/MealgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mealgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
